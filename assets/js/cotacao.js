1e261ca9dfabf23c8b1deb1b9fdd57d9d98d8cd9
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-02-18 13:21:54 -0300

    atualizando as cores style.css

--- assets/js/cotacao.js
+++ assets/js/cotacao.js
@@ -35,22 +35,14 @@ function meuGrafico(dados){
                 label: '# Reais',
                 data: [dolar,euro,],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(0, 255, 127, 0.5)',
+                    'rgba(0, 191, 255, 0.5)',
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(0, 100, 0, 1)',
+                    'rgba(70, 130, 180, 1)',
                 ],
-                borderWidth: 1
+                borderWidth: 2
             }]
         },
         options: {
